c1c400b64a6f7829402f3ada92fc327e5a7ba186
Saleem Raza <112764339+codingwithsaleem@users.noreply.github.com>
2024-11-14 12:48:36 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,9 +8,9 @@ const Navbar = () => {
     <div>
       <header className="text-gray-600 body-font">
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+    <Link className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
       <span className="ml-3 text-xl">Portfoilo</span>
-    </a>
+    </Link>
     <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
       <Link href='/' className="mr-5 hover:text-Black-100">Home</Link>
       <Link href='#' className="mr-5 hover:text-Black-100">About</Link>
@@ -19,12 +19,12 @@ const Navbar = () => {
       <Link href='#' className="mr-5 hover:text-Black-100">Projects</Link>
       <Link href='#' className="mr-5 hover:text-Black-100">Contact</Link>
     </nav>
-    <a href="https://milestone1-static-resume-gamma.vercel.app/" target="_blank">
+    <Link href="https://milestone1-static-resume-gamma.vercel.app/" target="_blank">
     <button className=" inline-flex items-center bg-blue-400 border-0 rounded-sm py-1 px-3 focus:outline-none hover:bg-white text-base mt-4 md:mt-0">
         Veiw Resume
         <TbHandClick className='text-xl ml-1'/>
     </button>
-    </a>
+    </Link>
   </div>
 </header>
     </div>
